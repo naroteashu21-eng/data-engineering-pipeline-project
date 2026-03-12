82268f67797806189b2595e02e620ca3736d3a84
naroteashu21-eng <naroteashu21@gmail.com>
2026-03-12 17:24:28 +0530

    Add data_pipeline.py script for ETL pipeline

--- scripts/data_pipeline.py
+++ scripts/data_pipeline.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+# Extract
+data = pd.read_csv('data/sales_data.csv')
+
+# Transform
+data['total_price'] = data['price'] * data['quantity']
+
+# Load
+data.to_csv('data/processed_sales_data.csv', index=False)
+
+print("Data pipeline executed successfully")
